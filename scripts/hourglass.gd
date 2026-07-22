6e6b82003d5bdd3e077993a026bda0a951b45466
Dungewar <dungewar@gmail.com>
2026-07-22 16:54:41 -0700

    Attempt to add hourglass clicking

---
+++ scripts/hourglass.gd
@@ -0,0 +1,16 @@
+extends Area2D
+class_name Hourglass
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _input_event(viewport: Viewport, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == 0 && event.pressed:
+			print("Clicked!")
