fa4a022dc1b01025e1e67298a5a1d02eef8e5ca7
thegrapeape22 <rohan.nadkarni123@gmail.com>
2026-07-22 18:08:22 -0700

    Create scene with hourglass that flips on click

--- scripts/hourglass.gd
+++ scripts/hourglass.gd
@@ -1,10 +1,11 @@
 extends Area2D
 class_name Hourglass
 
+var target_rotation: float = 0.0
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -12,5 +13,7 @@ func _process(delta: float) -> void:
 
 func _input_event(viewport: Viewport, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton:
-		if event.button_index == 0 && event.pressed:
-			print("Clicked!")
+		if event.button_index == MOUSE_BUTTON_LEFT && event.pressed:
+			target_rotation += PI
+			print(target_rotation)
+			create_tween().tween_property(self, "rotation", target_rotation, 0.3)
