df6ac7c27ee0f88b1c218090615aefd1b807f2a8
thegrapeape22 <rohan.nadkarni123@gmail.com>
2026-07-24 15:26:01 -0700

    Edit minigame parts

--- scripts/minigame.gd
+++ scripts/minigame.gd
@@ -1,10 +1,23 @@
 extends Node2D
 class_name Minigame
 
+@onready var close_button:Button = $CloseButton
+@onready var background:Sprite2D = $Background
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	close_button.connect("button_up", close_minigame())
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func open_minigame():
+	self.show()
+	for child in self.get_children():
+		child.show()
+
+func close_minigame():
+	self.hide()
+	for child in self.get_children():
+		child.hide();
