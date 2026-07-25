1a7e78127a11c1ca9857c6f97db1cd92770e6655
Olgierd Matusiewicz <dungewar@gmail.com>
2026-07-24 19:34:38 -0700

    Make super minigames

--- scripts/minigames/minigame.gd
+++ scripts/minigames/minigame.gd
@@ -1,8 +1,8 @@
-extends Node2D
+extends Control
 class_name Minigame
 
-@onready var close_button:Button = $CloseButton
-@onready var background:Sprite2D = $Background
+#@onready var close_button:Button = $CloseButton
+#@onready var background:Sprite2D = $Background
 var is_open:bool = false:
 	set(value):
 		is_open = value
@@ -14,7 +14,7 @@ var is_open:bool = false:
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	is_open = false
+	is_open = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
