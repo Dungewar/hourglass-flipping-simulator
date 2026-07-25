afd9cec89b9603b574843c8b3824b24276b2842d
thegrapeape22 <rohan.nadkarni123@gmail.com>
2026-07-24 18:18:24 -0700

    do some cheese minigame stuff, and big refactors

---
+++ scripts/hourglass/hourglass_spawner.gd
@@ -0,0 +1,34 @@
+extends Node2D
+class_name HourglassSpawner
+
+var hourglass_scene: PackedScene = preload("res://objects/hourglass.tscn")
+
+# Graphics
+var hourglass_width = 200
+var hourglass_start_position = -500
+
+func _ready():
+	GM.hourglass_spawner = self
+
+func _process(delta: float) -> void:
+	# moved test hourglass spawning to game_player.gd
+	pass
+
+func add_hourglass() -> Hourglass:
+	var hourglass: Hourglass = hourglass_scene.instantiate();
+	hourglass.position = Vector2(hourglass_start_position+hourglass_width * GM.game_player.hourglass_list.size(), 0)
+	GM.game_player.hourglass_list.append(hourglass)
+	add_child(hourglass)
+	return hourglass
+
+func remove_hourglass(hourglass: Hourglass):
+	GM.game_player.hourglass_list.erase(hourglass)
+	hourglass.queue_free()
+	
+	# shift hourglasses to the left
+	for i in range(0, GM.game_player.hourglass_list.size()):
+		var target_position: Vector2 = Vector2(hourglass_start_position + hourglass_width * i, 0)
+		create_tween().tween_property(GM.game_player.hourglass_list.get(i), "position", target_position, 0.5).set_trans(Tween.TRANS_QUART)
+	
+	#if hourglass_list.size() > 0:
+		#hourglass_list.pop_back()
--- scripts/minigames/collect_cheese_minigame.gd
+++ scripts/minigames/collect_cheese_minigame.gd
@@ -1,10 +1,23 @@
 extends Minigame
+class_name CollectCheeseMinigame
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var cheese_scene: PackedScene = preload("res://objects/collect_cheese/cheese.tscn")
 
+var time_since_spawn: float = 0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	time_since_spawn += delta
+	if time_since_spawn > 2:
+		spawn_cheese()
+		time_since_spawn = 0
+
+func spawn_cheese():
+	print('spawning')
+	var cheese: Cheese = cheese_scene.instantiate()
+	add_child(cheese)
+	var screen_size = get_viewport_rect().size
+	cheese.position = Vector2(randf_range(0.0, screen_size.x), randf_range(0.0, screen_size.y))
+
+func on_collect(cheese: Cheese):
+	print('collected')
