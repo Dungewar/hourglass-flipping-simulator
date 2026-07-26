b55037b7e8d332c76f9f612005bcbd192198a572
thegrapeape22 <rohan.nadkarni123@gmail.com>
2026-07-26 09:26:05 -0700

    add level selection to menu

--- scripts/UI/main_menu.gd
+++ scripts/UI/main_menu.gd
@@ -1,5 +1,27 @@
 extends Control
 class_name MainMenu
 
+var selected_level: int
+
+func _ready() -> void:
+	select_level(1)
+
 func _on_play_game_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/menus/game.tscn")
+
+@onready var buttons:HBoxContainer = $MarginContainer/VBoxContainer/Buttons
+
+func _on_level_1_pressed() -> void:
+	select_level(1)
+
+func _on_level_2_pressed() -> void:
+	select_level(2)
+
+func _on_level_3_pressed() -> void:
+	select_level(3)
+
+func select_level(level_number: int) -> void:
+	for i in range(len(buttons.get_children())):
+		buttons.get_children()[i].text = 'Level %d' % (i+1)
+	buttons.get_children()[level_number-1].text = '[Selected] Level %d' % (level_number)
+	selected_level = level_number
